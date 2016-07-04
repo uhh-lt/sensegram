fa0080025f983ad63f48aea47d437abbb12d7d11
Maria Pelevina <m.pelevina@gmail.com>
2016-07-04 20:49:38 +0200

    deleted old files

--- experiment/runs/26.02_new_models/sz300-w8-cb1-it1.sh
+++
@@ -1,5 +0,0 @@
-time ./pooling.py intermediate/ukwac-clusters-dep-cw-e0-N200-n200-minsize5-count3488990.csv 3488990 model/res2/corpus_en.norm-sz300-w8-cb1-it1-min20.w2v model/res2/corpus_en.norm-sz300-w8-cb1-it1-min20.w2v.senses -lowercase 
-time ./prediction.py context-eval/data/Dataset-SemEval-2013-13.csv model/res2/corpus_en.norm-sz300-w8-cb1-it1-min20.w2v.senses model/res2/corpus_en.norm-sz300-w8-cb1-it1-min20.w2v.contexts eval/res2/corpus_en.norm-sz300-w8-cb1-it1-min20_SemEval-2013-13_predictions_nothr.csv -lowercase
-cd context-eval/ 
-time ./semeval_2013_13.sh semeval_2013_13/keys/gold/all.key ../eval/res2/corpus_en.norm-sz300-w8-cb1-it1-min20_SemEval-2013-13_predictions_nothr.csv > ../eval/res2/corpus_en.norm-sz300-w8-cb1-it1-min20_SemEval-2013-13_predictions_nothr.csv.eval
-cd ..
